9dfb4d58f51095a110f54ee74df51874712a03ff
Yatendra Awana <yka6739@gmail.com>
2024-05-28 14:42:25 +0530

    Add project files.

---
+++ MauiApp1/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1
+{
+    public partial class AppShell : Shell
+    {
+        public string CopyrightText { get; }
+        public AppShell()
+        {
+            InitializeComponent();
+            // Set the current year and copy right text
+            CopyrightText = $"© {DateTime.Now.Year} Yatendra Kumar Awana";
+
+            // Binding context for the Shell
+            this.BindingContext = this;
+        }
+    }
+}
---
+++ MauiApp1/ContactUs.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class ContactUs : ContentPage
+{
+	public ContactUs()
+	{
+		InitializeComponent();
+	}
+}
